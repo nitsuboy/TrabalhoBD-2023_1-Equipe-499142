a3fe52c23a151d0a16699ecf14235b808f0607e9
Gabriel Augusto <Gabriel.afg182@gmail.com>
2023-06-28 13:16:28 -0300

    Update

---
+++ TBD_Biblioteca/Consulta_alunos_adm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TBD_Biblioteca
+{
+    public partial class Consulta_alunos_adm : Form
+    {
+        public Consulta_alunos_adm()
+        {
+            InitializeComponent();
+        }
+    }
+}
