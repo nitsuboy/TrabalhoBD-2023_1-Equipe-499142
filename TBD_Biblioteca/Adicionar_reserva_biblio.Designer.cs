31105ba653472301a7fea9c6ecc045f6232d23de
lucasg0mes <fcolucasgomes@alu.ufc.br>
2023-07-04 19:20:42 -0300

    last one

--- TBD_Biblioteca/Adicionar_reserva_biblio.Designer.cs
+++ TBD_Biblioteca/Adicionar_reserva_biblio.Designer.cs
@@ -28,81 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.SuspendLayout();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            comboBox1 = new ComboBox();
+            button2 = new Button();
+            label1 = new Label();
+            SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(62, 123);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.PlaceholderText = "DIgite a matrícula";
-            this.textBox1.Size = new System.Drawing.Size(133, 23);
-            this.textBox1.TabIndex = 0;
+            textBox1.Location = new Point(71, 164);
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "DIgite a matrícula";
+            textBox1.Size = new Size(151, 27);
+            textBox1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(388, 128);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(7, 6);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            button1.Location = new Point(443, 171);
+            button1.Name = "button1";
+            button1.Size = new Size(8, 8);
+            button1.TabIndex = 1;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
             // 
             // comboBox1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Aluno",
-            "Professor",
-            "Funcionário"});
-            this.comboBox1.Location = new System.Drawing.Point(62, 87);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(133, 23);
-            this.comboBox1.TabIndex = 2;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Aluno", "Professor", "Funcionário" });
+            comboBox1.Location = new Point(71, 116);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(151, 28);
+            comboBox1.TabIndex = 2;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(62, 161);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(133, 30);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Efetuar empréstimo";
-            this.button2.UseVisualStyleBackColor = true;
+            button2.Location = new Point(71, 215);
+            button2.Name = "button2";
+            button2.Size = new Size(152, 40);
+            button2.TabIndex = 3;
+            button2.Text = "Efetuar empréstimo";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(37, 54);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(197, 21);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Selecione o tipo de usuário";
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(42, 72);
+            label1.Name = "label1";
+            label1.Size = new Size(249, 28);
+            label1.TabIndex = 4;
+            label1.Text = "Selecione o tipo de usuário";
             // 
             // reserva
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(264, 247);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.Name = "reserva";
-            this.Text = "Adicionar_reserva_biblio";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(302, 329);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(comboBox1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "reserva";
+            Text = "Adicionar_reserva_biblio";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
